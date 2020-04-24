36f46edc00477001fc8749352bd26a40fd06aedc
bgroup11 <sarin17896@gmail.com>
2020-04-24 11:48:09 +0300

    Add project files.

---
+++ Tashbetzometry/Controllers/UserController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Tashbetzometry.Models;
+
+namespace Tashbetzometry.Controllers
+{
+	public class UserController : ApiController
+	{
+		// GET api/User/mail/password
+		[HttpGet]
+		[Route("api/User/{mail}")]
+		public User Get(string mail)
+		{
+			User u = new User();
+			return u.GetForgetPassFromDB(mail);
+		}
+
+		// GET api/User/mail/password
+		[HttpGet]
+		[Route("api/User/{mail}/{password}")]
+		public User Get(string mail, string password)
+		{
+			User u = new User();
+			return u.GetUserFromDB(mail, password);
+		}
+
+		// POST api/<controller>
+		public void Post([FromBody]User user)
+		{
+			User u = new User();
+			u.InsertUserToServer(user);
+		}
+
+		// PUT api/<controller>/5
+		public void Put([FromBody]User user)
+		{
+			User u = new User();
+			u.UpdatePasswordDB(user);
+		}
+
+		// DELETE api/<controller>/5
+		public void Delete(int id)
+		{
+		}
+	}
+}
---
+++ Tashbetzometry/Models/User.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Tashbetzometry.Models.DAL;
+
+namespace Tashbetzometry.Models
+{
+	public class User
+	{
+		string mail;
+		string userName;
+		string password;
+		string firstName;
+		string lastName;
+		string image;
+
+		public User()
+		{
+
+		}
+
+		public User(string mail, string userName, string password, string firstName, string lastName, string image)
+		{
+			Mail = mail;
+			UserName = userName;
+			Password = password;
+			FirstName = firstName;
+			LastName = lastName;
+			Image = image;
+		}
+
+		public User(string mail, string password)
+		{
+			Mail = mail;
+			Password = password;
+		}
+
+		public string Mail { get => mail; set => mail = value; }
+		public string Password { get => password; set => password = value; }
+		public string UserName { get => userName; set => userName = value; }
+		public string FirstName { get => firstName; set => firstName = value; }
+		public string LastName { get => lastName; set => lastName = value; }
+		public string Image { get => image; set => image = value; }
+
+		//login
+		public User GetUserFromDB(string mail, string password)
+		{
+			DBService db = new DBService();
+			return db.GetUserFromDB(mail, password);
+		}
+
+		//register
+		public int InsertUserToServer(User user)
+		{
+			DBService db = new DBService();
+			int numAffected = db.InsertUserToDB(user);
+			return numAffected;
+		}
+
+		//forgetPassword
+		public User GetForgetPassFromDB(string mail)
+		{
+			DBService db = new DBService();
+			return db.GetForgetPassFromDB(mail);
+		}
+
+		//update paassword
+		public int UpdatePasswordDB(User user)
+		{
+			DBService db = new DBService();
+			return db.UpdatePasswordDB(user);
+		}
+	}
+}
